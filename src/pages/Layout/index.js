7abf761b271e474b885bbe4b47d6f2bd399faf2b
Lu <stupidlifewww@foxmail.com>
2024-09-14 20:34:36 +0100

    month-date selector

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,24 +1,58 @@
-import { Outlet } from "react-router-dom";
-import { Button } from "antd-mobile";
+import { TabBar } from "antd-mobile";
 import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchBillList } from "@/store/modules/billSlice";
+import "./index.scss";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
+
 const Layout = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchBillList());
   }, [dispatch]);
+
+  const navigate = useNavigate();
+  const switchRoute = (path) => {
+    console.log(path);
+    navigate(path);
+  };
   return (
-    <div>
-      i am layout
-      <div>
-        <Button color="primary">全局</Button>
-        <div className="pink-theme">
-          <Button color="primary">局部</Button>
-        </div>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
       </div>
-      <Outlet />
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   );
 };
+
 export default Layout;
--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,4 +1,66 @@
+import { NavBar, DatePicker } from "antd-mobile";
+import "./index.scss";
+import { useState } from "react";
+import classNames from "classnames";
 const Month = () => {
-  return <div>i am Month</div>;
+  //open or close date selector
+  const [visibleDate, setVisibleDate] = useState(false);
+  const onConfirm = () => {
+    setVisibleDate(false);
+  };
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div
+            className="date"
+            onClick={() => {
+              setVisibleDate(true);
+            }}
+          >
+            <span className="text">月账单</span>
+            <span className={visibleDate ? "arrow" : "arrow expand"}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className="twoLineOverview">
+            <div className="item">
+              <span className="money">100</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">200</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            max={new Date()}
+            visible={visibleDate}
+            onClose={() => {
+              setVisibleDate(false);
+            }}
+            onCancel={() => {
+              setVisibleDate(false);
+            }}
+            onConfirm={onConfirm}
+          />
+        </div>
+        {/* 单日列表统计 */}
+        {100}
+      </div>
+    </div>
+  );
 };
+
 export default Month;
