d5852f0ed2f2ef9e96b374facf3b798a7dac91ed
Lu <stupidlifewww@foxmail.com>
2024-09-14 22:46:31 +0100

    Calculate statistics for the month

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -3,10 +3,12 @@ import "./index.scss";
 import { useState, useMemo } from "react";
 import dayjs from "dayjs";
 import { useSelector } from "react-redux";
-import _ from "loadsh";
+import _ from "lodash";
 const Month = () => {
   //1. open or close date selector
   const [visibleDate, setVisibleDate] = useState(false);
+  // 4.1 new a selected state
+  const [groupMonthBillList, setGroupMonthBillList] = useState([]);
   // 2.format time state
   const [time, setTime] = useState(() => {
     return dayjs().format("YYYY-MM");
@@ -14,8 +16,37 @@ const Month = () => {
 
   const onConfirm = (date) => {
     setVisibleDate(false);
-    setTime(dayjs(date).format("YYYY-MM"));
+    const formatTime = dayjs(date).format("YYYY-MM");
+    setTime(formatTime);
+    // 4.2 click comfirm button, get current month
+    // use the date as key, find the match month group (lodash)
+    // Ensure that monthGroupBill[formatTime] is an array
+    const list = monthGroupBill[formatTime] || [];
+    setGroupMonthBillList(list);
   };
+  // 4.3 calculate useMemo
+  const monthGroupResult = useMemo(() => {
+    // set up default value
+    if (!Array.isArray(groupMonthBillList)) {
+      console.error("groupMonthBillList is not an array:", groupMonthBillList);
+      return {
+        totalPay: 0,
+        totalReceive: 0,
+        balance: 0,
+      };
+    }
+    const pay = groupMonthBillList
+      .filter((item) => item.type === "pay")
+      .reduce((a, c) => a + c.money, 0);
+    const income = groupMonthBillList
+      .filter((item) => item.type === "income")
+      .reduce((a, c) => a + c.money, 0);
+    return {
+      pay,
+      income,
+      total: pay + income,
+    };
+  }, [groupMonthBillList]);
   /* 
   3.bill data groupd by month
     -get data from store
@@ -29,8 +60,15 @@ const Month = () => {
     // return 出计算后的值
     return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY-MM"));
   }, [billList]);
-  console.log(monthGroupBill, 11);
 
+  /* 
+  4.Calculate statistics for the month
+    -new a selected state
+    -click comfirm button, get current month
+    -use the date as key, find the match month group 
+    -calculate useMemo
+    -render
+  */
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -51,15 +89,17 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <span className="money">100</span>
+              <span className="money">{monthGroupResult.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">200</span>
+              <span className="money">
+                {monthGroupResult.income.toFixed(2)}
+              </span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthGroupResult.total.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
